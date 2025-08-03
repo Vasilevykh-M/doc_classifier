8e24bac638aafc04b4f2b395ee7e8d94b548f372
vasilevyh <vasilevyh00@trafficdata.ru>
2025-08-03 19:29:53 +0500

    update utils.py

--- contrastive/model/utils.py
+++ contrastive/model/utils.py
@@ -1,5 +1,5 @@
 import open_clip
 
-def get_processor(model_path='contrastive/tulip-B-16-224.ckpt'):
+def get_processor(model_path='tulip-B-16-224.ckpt'):
     _, _, preprocess = open_clip.create_model_and_transforms('TULIP-B-16-224', pretrained=model_path)
     return preprocess
